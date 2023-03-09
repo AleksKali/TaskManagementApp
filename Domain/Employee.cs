250732ee851c958ca004a6dc06a2f9da9a2d0512
AleksKali <kalimancevic.aleks@gmail.com>
2023-03-09 13:14:20 +0100

    Domain

---
+++ Domain/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ Domain/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum TaskStatus
+    {
+        Ready = 0,
+        InProgress = 1,
+        Done = 2,
+        Canceled= 3
+    }
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Employee Assignee { get; set; }
+        public Project Project { get; set; }
+        public DateTime DueDate { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+}
