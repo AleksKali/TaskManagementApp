179e1da4e47a551aec1ef355f38d3e89964f3be3
AleksKali <kalimancevic.aleks@gmail.com>
2023-03-12 20:57:56 +0100

    Adjustment of View for efficiency

--- UserInterface/UserControls/Employee/UCEmployeeKPI.Designer.cs
+++ UserInterface/UserControls/Employee/UCEmployeeKPI.Designer.cs
@@ -39,6 +39,7 @@ namespace UserInterface.UserControls.Employee
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeeKPI)).BeginInit();
             this.SuspendLayout();
             // 
@@ -95,10 +96,10 @@ namespace UserInterface.UserControls.Employee
             this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.label5.Location = new System.Drawing.Point(100, 483);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(883, 17);
+            this.label5.Size = new System.Drawing.Size(913, 17);
             this.label5.TabIndex = 8;
-            this.label5.Text = "*If the value of efficiency is 0.80 that means that the employee completed the ta" +
-    "sk in 80% of the estimated time it should take to complete it.";
+            this.label5.Text = "*If the value of Efficiency is 0.80 that means that the employee completed his ta" +
+    "sks in 80% of the estimated time it should take to complete them.";
             // 
             // label6
             // 
@@ -140,10 +141,22 @@ namespace UserInterface.UserControls.Employee
             this.label9.TabIndex = 11;
             this.label9.Text = "Task Finish date: date ";
             // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label10.Location = new System.Drawing.Point(119, 93);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(88, 17);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "*for one task";
+            // 
             // UCEmployeeKPI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label7);
@@ -173,5 +186,6 @@ namespace UserInterface.UserControls.Employee
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
